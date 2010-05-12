ce36158a54c4c9ac2e5846b572c11f28ce893e17
batteuxs <stephane.batteux@utt.fr>
2010-05-12 16:56:59 +0200

    Add connection to Ldap and tests related to LdapUser

---
+++ app/models/Ldap.java
@@ -0,0 +1,181 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package models;
+
+/**
+ *
+ * @author Administrateur
+ */
+import java.util.Enumeration;
+import java.util.Hashtable;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.naming.Context;
+import javax.naming.NamingEnumeration;
+import javax.naming.NamingException;
+import javax.naming.AuthenticationException;
+import javax.naming.directory.Attribute;
+import javax.naming.directory.Attributes;
+import javax.naming.directory.BasicAttribute;
+import javax.naming.directory.BasicAttributes;
+import javax.naming.directory.DirContext;
+import javax.naming.directory.InitialDirContext;
+import javax.naming.directory.SearchControls;
+import javax.naming.directory.SearchResult;
+
+public class Ldap {
+
+	/**
+	 * @param args
+	 */
+    Hashtable<String, String> ldapEnv =  new  Hashtable<String, String>();
+    String ldap_account;
+
+    public static void listerAttributs(Attributes atts) {
+		try {
+			for (NamingEnumeration e = atts.getAll(); e.hasMore();) {
+				Attribute a = (Attribute) e.next();
+				System.out.println(a.getID() + ":");
+				Enumeration values = a.getAll();
+				while (values.hasMoreElements()) {
+					System.out.println("valeur : " + values.nextElement().toString());
+				}
+			}
+		} catch (javax.naming.NamingException e) {
+			System.out.println(e.getMessage());
+		}
+	}
+    public  void addUser(Hashtable ldapEnv, String mail, String givenName, String sn, String cn, String userPassword){
+    	
+        Attributes attributes = new BasicAttributes(true); 
+        attributes.put(new BasicAttribute("mail", mail));
+        attributes.put(new BasicAttribute("objectClass", "inetOrgPerson"));
+       // attributes.put(new BasicAttribute("objectClass","simpleSecurityObject"));
+        attributes.put(new BasicAttribute("givenName", givenName));
+        attributes.put(new BasicAttribute("sn", sn));
+        attributes.put(new BasicAttribute("cn", cn));
+        attributes.put(new BasicAttribute("userPassword", userPassword));
+
+        DirContext ldapContext = null;
+        try {
+          ldapContext = new InitialDirContext(ldapEnv);
+         // MonObjet objet = new MonObjet("valeur1","valeur2");
+         ldapContext.bind("cn="+cn+",dc=hypertopic,dc=org",null, attributes);
+          ldapContext.close();
+
+        } catch (Exception e) {
+          System.err.println("Erreur lors de l'acces au serveur LDAP " + e);
+          e.printStackTrace();
+        }
+        System.out.println("fin des traitements");
+    }
+    public void deleteUser(Hashtable ldapEnv,String account){
+      DirContext ldapContext = null;
+      try {
+          ldapContext = new InitialDirContext(ldapEnv);
+          ldapContext.unbind(account);
+          ldapContext.close();
+        } catch (NamingException e) {
+          System.err.println("Erreur lors de l'acces au serveur LDAP" + e);
+          e.printStackTrace();
+        }
+        System.out.println("fin des traitements");
+      }
+
+    public void SetEnv(String ldap_server, String ldap_account, String ldap_password){
+
+        
+        ldapEnv.put(Context.INITIAL_CONTEXT_FACTORY, "com.sun.jndi.ldap.LdapCtxFactory");
+        ldapEnv.put(Context.PROVIDER_URL, "ldap://" + ldap_server);	// localhost:389
+        ldapEnv.put(Context.SECURITY_AUTHENTICATION, "simple");		// simple, none, list of SASL mechanisms
+        ldapEnv.put(Context.SECURITY_PRINCIPAL, ldap_account);			// cn=admin,dc=placeoweb
+        ldapEnv.put(Context.SECURITY_CREDENTIALS, ldap_password);		// motdepasse
+        this.ldap_account=ldap_account;
+     }
+
+    public Hashtable<String, String> getLdapEnv() {
+        return ldapEnv;
+    }
+
+    public Attributes getUserInfo(Hashtable ldapEnv, String login){
+
+        try {
+            DirContext ldapContext = null;
+            ldapContext = new InitialDirContext(ldapEnv);
+            System.out.println("LDAP : Bind Ok = ");
+            // Recherche en profondeur
+            // http://www.dil.univ-mrs.fr/~massat/ens/jee/ldap.html
+            SearchControls controle = new SearchControls();
+            controle.setSearchScope(SearchControls.SUBTREE_SCOPE);
+            //			 String critere = "(|(sn=premier)(sn=deux*))";
+            //			 String critere = "(cn = *)";	// ne trouve rien
+            String critere = "(cn="+login+")"; // ok					ldapsearch -x -h localhost -b 'dc=placeoweb' '(cn=*)'
+            //			 DirContext ictx = new InitialDirContext(ldapEnv);
+            //			 NamingEnumeration<SearchResult> e = ldapContext.search("ou=monorganisationunit,dc=placeoweb", critere, controle);
+            NamingEnumeration<SearchResult> e = ldapContext.search("dc=hypertopic,dc=org", critere, controle);
+            while (e.hasMore()) {
+                SearchResult r = e.next();
+                System.out.println("name: " + r.getName());
+                System.out.println("object: " + r.getClassName());
+                System.out.println("getAttributes: " + r.getAttributes());
+                //System.out.println("bigsiri: " + r.getAttributes().get("userPassword").getID());
+                //listerAttributs(r.getAttributes());
+				
+				return r.getAttributes();
+            }
+			
+        }catch(AuthenticationException error){
+        	return null;
+        	
+        }catch (NamingException ex) {
+            Logger.getLogger(Ldap.class.getName()).log(Level.SEVERE, null, ex);
+        }
+		
+		return null;
+    }
+	
+	
+    public void modifyPassword(Hashtable ldapEnv, String password){
+        try {
+            DirContext ldapContext = null;
+            ldapContext = new InitialDirContext(ldapEnv);
+
+              Attributes attributes = new BasicAttributes(true);
+              Attribute attribut = new BasicAttribute("userPassword");
+              attribut.add(password);
+              attributes.put(attribut);
+
+              ldapContext.modifyAttributes(this.ldap_account,
+                    DirContext.REPLACE_ATTRIBUTE,attributes); 
+              ldapContext.close();
+
+        } catch (NamingException ex) {
+            Logger.getLogger(Ldap.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+	
+	public void modifyAttribute(Hashtable ldapEnv, String login, String attributeName, String value){
+        try {
+            DirContext ldapContext = null;
+            ldapContext = new InitialDirContext(ldapEnv);
+			
+              Attributes attributes = new BasicAttributes(true);
+              Attribute attribut = new BasicAttribute(attributeName);
+              attribut.add(value);
+              attributes.put(attribut);
+
+              ldapContext.modifyAttributes("cn="+login+",dc=hypertopic,dc=org",
+                    DirContext.REPLACE_ATTRIBUTE,attributes); 
+              ldapContext.close();
+
+        } catch (NamingException ex) {
+            Logger.getLogger(Ldap.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+  }
