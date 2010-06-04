f049532b0a5bb41523d5377298e55c6fda97bdc9
bricetchiebeb <bricetchiebeb[at]users.sourceforge.net>
2010-06-04 14:30:09 +0200

    Correct version

--- app/controllers/Invitation.java
+++ app/controllers/Invitation.java
@@ -12,6 +12,29 @@ import play.data.validation.*;
 public class Invitation extends Controller {
 
     	
+	public static void inviteNewMember(@Required String nom,@Required String prenom, @Required String mail,
+										@Required String langue){
+	
+		String signature ="hash";
+		String message="";
+		String sender="testSendInvitation@hypertopic-test.com";
+		
+		if (validation.hasErrors()){
+				render("Application/invitation.html");
+		}
+		else{
+			if(langue.equals("fr")){
+				message = " Vous avez été invité(e) par votre parrain à rejoindre la communauté hypertopic. Pour ce faire, veuillez vous inscrire en cliquant ici:";//Lien.signature;
+			}else if(langue.equals("en")){
+				message = "You have been invited by your godfather to register as a member to the Hypertopic community.";//Lien.signature;
+			}
+			
+			Mail.send(sender, "megcha5002@gmail.com","sujet", message);
+			flash.success("Your invitation has been sent successfully!");
+			Application.invitation();
+		}
+	
+	}
 	
 	public static void sendInvitation( String firstNameSender,
 								String lastNameSender,
@@ -26,7 +49,7 @@ public class Invitation extends Controller {
 		String sender="yessoufy@gmail.com";
 		
 		if (validation.hasErrors()){
-				render("Application/show.html");
+				render("Application/invitation.html");
 		}else{
 			if(msgLang.equals("fr")){
 				message = " Vous avez été invité(e) par "+firstNameSender+" "+lastNameSender+"à rejoindre la communauté hypertopic. Pour ce faire, veuillez vous inscrire en cliquant ici:";//Lien.signature;
@@ -34,7 +57,7 @@ public class Invitation extends Controller {
 				message = "You have been invited by "+firstNameSender+" "+lastNameSender+" to register as a member to the Hypertopic community.";//Lien.signature;
 			}
 			
-			Mail.send(sender, "essoufy_@hotmail.com","sujet", message);
+			//Mail.send(sender, "essoufy_@hotmail.com","sujet", message);
 			flash.success("Your invitation has been sent successfully!");
 		}
 		//show(); la vue pour afficher les erreur ou le succes de l'envoi d'invitation
