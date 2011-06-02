29883cb19fb88036197983b1411ab2f2471165d1
Guillaume Radet <guillaume.radet@utt.fr>
2011-06-02 19:52:05 +0200

    fix : nullPointerException for invitation : solved

--- app/controllers/Invitation.java
+++ app/controllers/Invitation.java
@@ -62,41 +62,12 @@ public class Invitation extends BaseController {
             	firstNameGodfather="l'administrateur";
             	mailGodfather="Hypertopic Team <noreply@hypertopic.org>";
             }
-            HashMap<String, String> infos=Ldap.getConnectedUserInfos(session.get("username"));
-            mailGodfather=infos.get("mail");
-            firstNameGodfather=infos.get("firstName");
-            lastNameGodfather=infos.get("lastName");
-    		/*Ldap adminConnection = new Ldap();
-    		adminConnection.SetEnv(Play.configuration.getProperty("ldap.host"),Play.configuration.getProperty("ldap.admin.dn"), Play.configuration.getProperty("ldap.admin.password"));
-    		Attributes r=adminConnection.getUserInfo(adminConnection.getLdapEnv(),session.get("username"));
-	    	try{
-	    		NamingEnumeration  e=r.getAll();
-	    		while(e.hasMore()){
-	    			javax.naming.directory.Attribute a=(javax.naming.directory.Attribute)e.next();
-	    			String attributeName=a.getID();
-	    			String attributeValue="";
-	    			Enumeration values = a.getAll();
-	    			while(values.hasMoreElements()){
-	    				attributeValue = values.nextElement().toString();
-	    			}
-					if(attributeName.equals("mail"))
-					{
-						mailGodfather = attributeValue;
-					}
-					else if(attributeName.equals("givenName"))
-					{
-						firstNameGodfather=attributeValue;
-					}
-					else if(attributeName.equals("sn"))
-					{
-						lastNameGodfather = attributeValue;
-					}
-	    		}
-	    	}catch(javax.naming.NamingException e) {
-	    		System.out.println(e.getMessage());
-	    	}*/
-            //
-            //
+            else{
+            	HashMap<String, String> infos=Ldap.getConnectedUserInfos(session.get("username"));
+	            mailGodfather=infos.get("mail");
+	            firstNameGodfather=infos.get("firstName");
+	            lastNameGodfather=infos.get("lastName");
+            }
             System.out.println("invitenewmember");
             try {
                 url = "http://" + request.domain;
